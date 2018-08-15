f7d6e45b7d198d3c1c31b00afdb5ae5ab889f945
Artifex-Latronis <Artifex-Latronis@users.noreply.github.com>
2018-08-15 12:15:47 -0700

    implement HTTP params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +15,7 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule, // this is like 'commons', except this one only and must exist in the app.module.
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -11,21 +11,26 @@ import { AuthService } from '../auth/auth.service';
 })
 export class DataStorageService {
 
-  constructor (private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
+  constructor (private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put('https://recipeapp-55c9e.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    // this way is having your parameters in the url
+    // return this.httpClient.put('https://recipeapp-55c9e.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+
+    // this way is using the httpClientModule and using the params object to pass them, this keeps your URL's short
+    return this.httpClient.put('https://recipeapp-55c9e.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+      params: new HttpParams().set('auth', token)
+    });
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://recipeapp-55c9e.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://recipeapp-55c9e.firebaseio.com/recipes.json?auth=' + token)
       .pipe(map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipe);
